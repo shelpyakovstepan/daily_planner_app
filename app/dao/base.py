24c1ed33fe27606dc7850cfe322b02f8587867a0
shelpyakovstepan <step3210shelpyakov@gmail.com>
2025-06-10 15:55:32 +0300

    Добавлены базовые методы для работы с бд (нахождение по id, нахождение одного или ничего, нахождение всего, ддобавление и удаление)

---
+++ app/dao/base.py
@@ -0,0 +1,45 @@
+from sqlalchemy import insert, select, delete
+
+from app.database import async_session_maker
+
+
+class BaseDao:
+    model = None
+
+    @classmethod
+    async def add(cls, **values):
+        async with async_session_maker() as session:
+            query = insert(cls.model).values(**values)
+            session.execute(query)
+            session.commit()
+
+    @classmethod
+    async def find_by_id(cls, model_id):
+        async with async_session_maker() as session:
+            query = select(cls.model).filter_by(id=model_id)
+            result = await session.execute(query)
+
+            return result.scalar_one_or_none()
+
+    @classmethod
+    async def find_all(cls, **values):
+        async with async_session_maker() as session:
+            query = select(cls.model).filter_by(**values)
+            result = await session.execute(query)
+
+            return result.scalars().all()
+
+    @classmethod
+    async def find_one_or_none(cls, **values):
+        async with async_session_maker() as session:
+            query = select(cls.model).filter_by(**values)
+            result = await session.execute(query)
+
+            return result.scalar_one_or_none()
+
+    @classmethod
+    async def delete(cls, **values):
+        async with async_session_maker() as session:
+            query = delete(cls.model).filter_by(**values)
+            session.execute(query)
+            session.commit()
