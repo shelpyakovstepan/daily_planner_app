1737f95e5d14a05fd4501c3c0ae3ecebea5cd2c6
shelpyakovstepan <step3210shelpyakov@gmail.com>
2025-06-12 13:18:38 +0300

    Добавлено начальное логирование

--- app/database.py
+++ app/database.py
@@ -2,12 +2,14 @@ from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 
 from app.config import settings
+from app.logger import logger
 
 DATABASE_URL = f"postgresql+asyncpg://{settings.DB_USER}:{settings.DB_PASS}@{settings.DB_HOST}:{settings.DB_PORT}/{settings.DB_NAME}"
 
 engine = create_async_engine(DATABASE_URL)
 
 async_session_maker = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+logger.info("Successfully connected to database.")
 
 class Base(DeclarativeBase):
     pass
--- app/main.py
+++ app/main.py
@@ -1,4 +1,8 @@
-from fastapi import FastAPI
+import time
+
+from fastapi import FastAPI, Request
+
+from app.logger import logger
 from app.users.router import router as users_router
 from app.entries.router import router as entries_router
 
@@ -6,3 +10,12 @@ app = FastAPI()
 
 app.include_router(users_router)
 app.include_router(entries_router)
+
+@app.middleware("http")
+async def add_process_time_header(request: Request, call_next):
+    start_time = time.perf_counter()
+    response = await call_next(request)
+    process_time = time.perf_counter() - start_time
+    response.headers["X-Process-Time"] = str(process_time)
+    logger.info(f"Request handling time: {round(process_time, 4)}")
+    return response
