66ec8263e5daa8a90b9631a7bd5751af92270661
shelpyakovstepan <step3210shelpyakov@gmail.com>
2025-06-25 21:05:26 +0300

    Исправил CI/CD

--- docker/celery.sh
+++ docker/celery.sh
@@ -1,8 +1,7 @@
-#!/bin/sh^M
-^M
+#!/bin/bash
+
 if [[ "${1}" == "celery" ]]; then
   celery -A app.tasks.celery_app:celery beat & celery -A app.tasks.celery_app:celery worker --loglevel=info --pool=solo
 elif [[ "${1}" == "flower" ]]; then
   celery -A app.tasks.celery_app:celery flower
 fi
-^M
