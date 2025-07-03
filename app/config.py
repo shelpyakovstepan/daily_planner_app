0be85d421ca96b64662f72e040672287bb6b70bc
shelpyakovstepan <step3210shelpyakov@gmail.com>
2025-07-03 20:20:39 +0300

    Переписал docker-compose.yml, переделал env_file

--- app/config.py
+++ app/config.py
@@ -10,7 +10,7 @@ class Settings(BaseSettings):
     MODE: Literal["DEV", "PROD", "TEST"]
     LOG_LEVEL: Literal["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"]
 
-    DB_HOST: str
+    DB_HOST: str = "${DB_HOST}"
     DB_PORT: int
     DB_USER: str
     DB_PASS: str
@@ -35,7 +35,7 @@ class Settings(BaseSettings):
     SECRET_KEY: str
     ALGORITHM: str
 
-   # model_config = SettingsConfigDict(
+   #model_config = SettingsConfigDict(
    #     env_file=os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", ".env")
    # )
 
