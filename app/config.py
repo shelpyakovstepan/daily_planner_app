c521a9e35445784fd86c88686b868d9508d1a9a5
shelpyakovstepan <step3210shelpyakov@gmail.com>
2025-07-03 20:12:38 +0300

    Переписал docker-compose.yml, переделал env

--- app/config.py
+++ app/config.py
@@ -35,9 +35,9 @@ class Settings(BaseSettings):
     SECRET_KEY: str
     ALGORITHM: str
 
-    model_config = SettingsConfigDict(
-        env_file=os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", ".env")
-    )
+   # model_config = SettingsConfigDict(
+   #     env_file=os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", ".env")
+   # )
 
 
 settings = Settings()  # pyright: ignore [reportCallIssue]
